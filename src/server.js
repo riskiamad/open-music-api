bf7a9fec2d640e6009d45322fcc1ea5943e3b8f5
Achmad Rizky Syahrani <riskiamad@gmail.com>
2022-10-17 00:26:24 +0700

    implement authentications

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const ClientError = require('./exceptions/ClientError');
+const TokenManager = require('./tokenize/TokenManager');
 
 // albums
 const albums = require('./api/albums');
@@ -17,10 +19,16 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
     const albumsService = new AlbumsService();
     const songsService = new SongsService();
     const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
     const server = Hapi.server({
         port: process.env.PORT,
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -31,28 +39,61 @@ const init = async () => {
         },
     });
 
+    // register plugin external
+    await server.register([
+        {
+            plugin: Jwt,
+        },
+    ]);
+
+    // mendefinisikan strategy autentikasi jwt
+    server.auth.strategy('open_music_jwt', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
+    });
+
     await server.register([
         {
             plugin: albums,
             options: {
                 service: albumsService,
                 validator: AlbumsValidator,
-            }
+            },
         },
         {
             plugin: songs,
             options: {
                 service: songsService,
                 validator: SongsValidator,
-            }
+            },
         },
         {
             plugin: users,
             options: {
                 service: usersService,
                 validator: UsersValidator,
-            }
-        }
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
+            },
+        },
     ]);
 
     server.ext('onPreResponse', (request, h) => {
